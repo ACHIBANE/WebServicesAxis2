d960b12171b0badd0453a8ca49e113ef0995009d
TOSHIBANE <achibane.abdelghani@gmail.com>
2016-12-19 18:57:23 +0000

    deploy, invoce, implement

--- src/wmd/ws/AuthService.java
+++ src/wmd/ws/AuthService.java
@@ -10,16 +10,16 @@ public class AuthService implements Serializable{
 	static HashMap<String, User> hmap;
 	static{
 		hmap = new HashMap<String,User>();
-		hmap.put( "user1",new User("achiban","azerty123"));
-		hmap.put("user2",new User("bouchra","azerty"));
+		hmap.put( "achiban",new User("achiban","azerty123"));
+		hmap.put("bouchra",new User("bouchra","azerty"));
 		hmap.put("user3",new User("anonyme","aqw123"));
 	}
 	public static User CheckUser (String login,String pswd){
-		User user=new User(login, pswd);
-		if(hmap.containsValue(user)){
-			return user;
-		}
-		return null;
+//		User user=new User(login, pswd);
+		if(hmap.containsKey(login)){
+			return hmap.get(login);
+		}else{
+		return null;}
 	}	
 	
 	
