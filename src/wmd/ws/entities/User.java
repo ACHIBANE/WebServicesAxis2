d919de1627ca742c69b2a3d13a07c13a2536e13b
TOSHIBANE <achibane.abdelghani@gmail.com>
2016-12-19 16:45:57 +0000

    nada

--- src/wmd/ws/entities/User.java
+++ src/wmd/ws/entities/User.java
@@ -1,5 +1,30 @@
 package wmd.ws.entities;
 
 public class User {
+	
+	Long userID;
+	String login;
+	String pswd;
+	
+	
+	public User(String login, String pswd) {
+		
+		this.login = login;
+		this.pswd = pswd;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPswd() {
+		return pswd;
+	}
+	public void setPswd(String pswd) {
+		this.pswd = pswd;
+	}
+	
+	
 
 }
