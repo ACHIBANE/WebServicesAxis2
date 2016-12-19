76caaac97b69e2324c4f2beed1443c18ce8df67b
TOSHIBANE <achibane.abdelghani@gmail.com>
2016-12-19 15:00:31 +0000

    Initialisation du projet

---
+++ src/wmd/ws/AuthService.java
@@ -0,0 +1,29 @@
+package wmd.ws;
+
+import java.util.HashMap;
+
+import wmd.ws.entities.User;
+
+public class AuthService {
+	
+static{
+		
+	HashMap<String, String> hmap = new HashMap<String,String>();
+	hmap.put("wmd1", "azerty1");
+	hmap.put("wmd2", "azerty2");
+	hmap.put("wmd3", "azerty3");
+	
+}
+	public User CheckUser (String login,String pswd){
+		
+		
+		return null;
+	}	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ src/wmd/ws/entities/User.java
@@ -0,0 +1,5 @@
+package wmd.ws.entities;
+
+public class User {
+
+}
