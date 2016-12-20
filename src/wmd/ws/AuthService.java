1015d3c24a14685e54c4e70a5860fbe84b92c03a
TOSHIBANE <achibane.abdelghani@gmail.com>
2016-12-20 20:53:17 +0000

    Creating asimple Hello Service

--- src/wmd/ws/AuthService.java
+++ src/wmd/ws/AuthService.java
@@ -7,25 +7,26 @@ import wmd.ws.entities.User;
 
 public class AuthService implements Serializable{
 	
-	static HashMap<String, User> hmap;
-	static{
-		hmap = new HashMap<String,User>();
-		hmap.put( "achiban",new User("achiban","azerty123"));
-		hmap.put("bouchra",new User("bouchra","azerty"));
-		hmap.put("user3",new User("anonyme","aqw123"));
+//	static HashMap<String, User> hmap;
+//	static{
+//		hmap = new HashMap<String,User>();
+//		hmap.put( "achiban",new User("achiban","azerty123"));
+//		hmap.put("bouchra",new User("bouchra","azerty"));
+//		hmap.put("user3",new User("anonyme","aqw123"));
+//	}
+//	public static User CheckUser (String login,String pswd){
+////		User user=new User(login, pswd);
+//		if(hmap.containsKey(login)){
+//			return hmap.get(login);
+//		}else{
+//		return null;}
+//	}	
+	
+	public static String Hello (String login){
+		
+			return ("hello "+ login);
+		
 	}
-	public static User CheckUser (String login,String pswd){
-//		User user=new User(login, pswd);
-		if(hmap.containsKey(login)){
-			return hmap.get(login);
-		}else{
-		return null;}
-	}	
-	
-	
-	
-	
-	
 	
 
 }
